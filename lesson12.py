f08c367e40e485945e05112e78f15839bbc3c9ae
Zalkar <azisuulu2002@gmail.com>
2019-12-02 18:35:34 +0600

    Add lesson12.py

--- lesson12.py
+++ lesson12.py
@@ -36,15 +36,46 @@ class Contact:
 
 
 class ContactList:
-	cotacts_list = []
+	contacts_list = []
 
-	def add_contact(self, contact):
+	def add_contact(self, contact): # Добовляем в список
 		self.contacts_list.append(contact)
 
-	def seacrh_contact(self, contact):
+	def seacrh_contact(self, contact): # Ищем контакт
 		for i in self.contacts_list:
-			
-	
+			if i == contact:
+				print(f'Name: {i.name} \nLast name: {i.last_name} \nNumber: {i.number}')
+
+	def remove_contact(self, contact): # Удаляем контакт
+		for i in self.contacts_list:
+			if i == contact:
+				self.contacts_list.remove(contact)
+				print('Сontact deleted')
+				print('____________________________')		
+
+	def all_contacts(self): # Показываем весь список
+		for i in self.contacts_list:
+			print(f'Name: {i.name} \nLast name: {i.last_name} \nNumber: {i.number}')
+			print('____________________________')		
 		
-pers = Contact('Nurs', '', '0777777')
-print(pers)
+cnt1 = Contact('Nurs', '', '0777777')
+cnt2 = Contact('Zalkar', '', '0705 42 01 72')
+cnt3 = Contact('Tolubai', '', '07075644565')
+cnt4 = Contact('Chyngyz', '', '05054654847')
+cnt5 = Contact('Stela', '', '055457564/98')
+
+cnlist = ContactList()
+cnlist.all_contacts()
+cnlist.add_contact(cnt1)
+cnlist.add_contact(cnt2)
+cnlist.add_contact(cnt3)
+cnlist.add_contact(cnt4)
+cnlist.add_contact(cnt5)
+cnlist.seacrh_contact(cnt1)
+print('''
+
+	''')
+cnlist.all_contacts()
+cnlist.remove_contact(cnt5)
+cnlist.remove_contact(cnt4)
+cnlist.all_contacts()
